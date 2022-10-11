c36b08f923ac154bfc19c2266356a02e68310162
GongYuQi <gongyuqi@rincers.com>
2022-10-11 16:31:41 +0800

    引入stylus并美化canvas样式

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
+import './style/index.styl';
 console.log('hello canvas');
--- webpack.config.client.js
+++ webpack.config.client.js
@@ -15,6 +15,15 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.styl$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "stylus-loader"
+        ]
       }
     ]
   }
